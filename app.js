03e46723fefca98d8cfc1feefe11fddd6cd01893
Hatice Sürümlü <hatice.surumlu@ogr.sakarya.edu.tr>
2022-08-28 22:39:32 +0300

    add container and image

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
+const container = document.querySelector(".container")
+const image = document.querySelector("#music-image")
+
+
+
+
 const player= new MusicPlayer(musicList);
 
 let music=player.getMusic();
-
-console.log(music.getName());
-
-player.next();
-music=player.getMusic();
 console.log(music.getName());
 
 
-player.previous();
-music=player.getMusic();
-console.log(music.getName());
-
-
-player.next();
-music=player.getMusic();
-console.log(music.getName());
