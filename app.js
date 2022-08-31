0071b72e746cdf5e29d2af4083965b902ce4c27a
Hatice Sürümlü <hatice.surumlu@ogr.sakarya.edu.tr>
2022-08-31 13:32:38 +0300

    add progress-bar, buttons and pauseMusic func

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const singer = document.querySelector('#music-details .singer')
 const prev = document.querySelector('#controls #prev')
 const play = document.querySelector('#controls #play')
 const next = document.querySelector('#controls #next')
-
+const duration= document.querySelector("#duration")
+const currentTime=document.querySelector("#current-time")
+const progressBar = document.querySelector("#progress-bar")
 
 
 
@@ -33,14 +35,14 @@ play.addEventListener("click", () => {
 
 });
 
-function pauseMusic() {
+const pauseMusic=()=> {
     container.classList.remove("playing");
     play.classList="fa-solid fa-play";
     audio.pause();
 
 }
 
-function playMusic() {
+const playMusic=()=> {
     container.classList.add("playing");
     play.classList="fa-solid fa-pause";
     audio.play();
@@ -51,7 +53,7 @@ prev.addEventListener("click",()=>{
 prevMusic();
 })
 
-function prevMusic(){
+const prevMusic=()=>{
     player.prev();
     let music=player.getMusic();
     displayMusic(music);
@@ -62,10 +64,49 @@ next.addEventListener("click",()=>{
 nextMusic();
 })
 
-function nextMusic(){
+const nextMusic=()=>{
     player.next();
     let music=player.getMusic();
     displayMusic(music);
     playMusic();
 
 }
+
+
+
+
+
+// const   calculateTime = (toplamSaniye) => {
+//     const dakika = Math.floor(toplamSaniye/60);
+//     const saniye= Math.floor(toplamSaniye%60);
+//     const guncellenenSaniye=saniye<10 ? ` 0${saniye}` : `${saniye} ` ;
+//     const sonuc=`${dakika}:${guncellenenSaniye}` ;
+//     return sonuc;
+// }
+
+
+const calculateTime = (toplamSaniye) => {
+    const dakika = Math.floor(toplamSaniye / 60);
+    const saniye = Math.floor(toplamSaniye % 60);
+    const guncellenenSaniye = saniye < 10 ? `0${saniye}`: `${saniye}`;
+    const sonuc = `${dakika}:${guncellenenSaniye}`;
+    return sonuc;
+}
+
+audio.addEventListener("loadedmetadata",()=>{
+    duration.textContent=calculateTime(audio.duration);
+    progressBar.max = Math.floor(audio.duration);
+    
+    });
+
+    audio.addEventListener("timeupdate",()=>{
+        progressBar.value=Math.floor(audio.currentTime);
+        currentTime.textContent=calculateTime(progressBar.value)
+
+    })
+
+    progressBar.addEventListener("input",()=>{
+currentTime.textContent=calculateTime(progressBar.value);
+audio.currentTime=progressBar.value;
+
+    })
--- music.js
+++ music.js
@@ -14,10 +14,10 @@ class Music {
 
 const musicList= [ 
 
-    new Music("Kerkük Zindanı","Cem Karaca","cem1.jpeg","vurun-ulan-vurun.mp3"),
-    new Music("Oy Gülüm","Cem Karaca","cem2.jpeg","raptiye.mp3"),
-    new Music("Islak Islak","Cem Karaca","3.jpeg","vurun-ulan-vurun.mp3"),
-    new Music("Deniz Üstü Köpürür","Cem Karaca","1.jpeg","1.mp3"),
+    new Music("33 Kurşun","Cem Karaca","cembBaba.jpg","33-kursun.mp3"),
+    new Music("Raptiye Rap Rap","Cem Karaca","cembBaba2.jpg","raptiye.mp3"),
+    new Music("Beni Bir Gözleri Ahuya Zebun Etti Felek","Cem Karaca","cembBaba3.jpg","ahu.mp3"),
+    new Music("Deniz Üstü Köpürür","Cem Karaca","cembBaba4.jpg","sen-de-basini-alip-gitme.mp3"),
     new Music("Raptiye Rap Rap","Cem Karaca","2.jpeg","2.mp3"),
     new Music("33 Kurşun","Cem Karaca","3.jpeg","3.mp3"),
     new Music("Tamirci Çırağı","Cem Karaca","1.jpeg","1.mp3"),
