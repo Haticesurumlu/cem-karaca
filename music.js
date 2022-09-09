71169be69a02a482ca4f695d04e43d17a49a3a3d
Hatice Sürümlü <hatice.surumlu@ogr.sakarya.edu.tr>
2022-09-09 09:29:24 +0300

    ref: redefined getName func

--- music.js
+++ music.js
@@ -7,19 +7,20 @@ class Music {
         this.file=file;
     }
     getName() {
-        return this.title + "-" + this.singer;
+        return this.title ;
     }
 }
 
 
 const musicList= [ 
-    new Music("Demedim mi Oy Gülüm Oy ","Cem Karaca","cemBaba-kardaslar.jpg","konserKaydi.mp3"),
-    new Music("33 Kurşun","Cem Karaca","cembBaba.jpg","33-kursun.mp3"),
-    new Music("Raptiye Rap Rap","Cem Karaca","cembBaba2.jpg","raptiye.mp3"),
-    new Music("Beni Bir Gözleri Ahuya Zebun Etti Felek","Cem Karaca","cembBaba3.jpg","ahu.mp3"),
-    new Music("Deniz Üstü Köpürür","Cem Karaca","cembBaba4.jpg","sen-de-basini-alip-gitme.mp3"),
+    new Music("Demedim mi / Oy Gülüm Oy ","Cem Karaca","cemBaba-kardaslar.jpg","konserKaydi.mp3"),
+    new Music("33 Kurşun","Cem Karaca","cembBaba2.jpg","33-kursun.mp3"),
+    new Music("Raptiye Rap Rap","Cem Karaca","cembBaba.jpg","raptiye.mp3"),
+    new Music("Beni Bir Gözleri Ahuya Zebun Etti Felek","Cem Karaca","cembBaba4.jpg","ahu.mp3"),
+    new Music("Sen de Başını Alıp Gitme","Cem Karaca","cembBaba3.jpg","sen-de-basini-alip-gitme.mp3"),
     new Music("Kerkük Zindanı","Cem Karaca","cemBaba-kerkük.jpg","kerkuk-zindani.mp3"),
-    new Music("33 Kurşun","Cem Karaca","askaros.jpg","askaros.mp3")
+    new Music("Askaros Deresi","Cem Karaca","askaros.jpg","askaros.mp3"),
+    new Music("Bu Son Olsun","Cem Karaca","cembBaba.jpg","")
 
     
 ];
