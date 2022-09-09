55ebf517a675964ceab219cb2af4245475b4a8e1
Hatice Sürümlü <hatice.surumlu@ogr.sakarya.edu.tr>
2022-09-09 20:12:14 +0300

    feat: add music and images

--- music.js
+++ music.js
@@ -15,12 +15,11 @@ class Music {
 const musicList= [ 
     new Music("Demedim mi / Oy Gülüm Oy ","Cem Karaca","cemBaba-kardaslar.jpg","konserKaydi.mp3"),
     new Music("33 Kurşun","Cem Karaca","cembBaba2.jpg","33-kursun.mp3"),
-    new Music("Raptiye Rap Rap","Cem Karaca","cembBaba.jpg","raptiye.mp3"),
     new Music("Beni Bir Gözleri Ahuya Zebun Etti Felek","Cem Karaca","cembBaba4.jpg","ahu.mp3"),
     new Music("Sen de Başını Alıp Gitme","Cem Karaca","cembBaba3.jpg","sen-de-basini-alip-gitme.mp3"),
-    new Music("Kerkük Zindanı","Cem Karaca","cemBaba-kerkük.jpg","kerkuk-zindani.mp3"),
+    new Music("Kerkük Zindanı","Cem Karaca","kerkuk.jpg","kerkuk-zindani.mp3"),
     new Music("Askaros Deresi","Cem Karaca","askaros.jpg","askaros.mp3"),
-    new Music("Bu Son Olsun","Cem Karaca","cembBaba.jpg","")
+    new Music("Bu Son Olsun","Cem Karaca","cembBaba.jpg","son-olsun.mp3")
 
     
 ];
